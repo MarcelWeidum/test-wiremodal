d51a44a45a722ec55f38d2ad964a3d258160b79e
marcelweidum <marcelweidum@live.nl>
2021-06-01 17:44:55 +0200

    Tailwind CSS purge safelist

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  purge: [
-    './vendor/livewire-ui/modal/resources/views/*.blade.php',
-    './storage/framework/views/*.php',
-    './resources/views/**/*.blade.php',
-  ],
+  purge: {
+    content: [
+      './vendor/livewire-ui/modal/resources/views/*.blade.php',
+      './storage/framework/views/*.php',
+      './resources/views/**/*.blade.php',
+    ],
+    options: {
+      safelist: [
+        'sm:max-w-2xl'
+      ]
+    }
+  },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
